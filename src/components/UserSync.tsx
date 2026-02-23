e8cb73634df4413a2289ded09e5de95feca6fab6
Jay8850jkl <shreyashpatil530@gmail.com>
2026-02-23 14:48:11 +0530

    Fix build error by adding convex codegen to build script and using path aliases

--- src/components/UserSync.tsx
+++ src/components/UserSync.tsx
@@ -3,7 +3,7 @@
 
 import { useUser } from "@clerk/nextjs";
 import { useMutation } from "convex/react";
-import { api } from "../../convex/_generated/api";
+import { api } from "@convex/_generated/api";
 import { useEffect } from "react";
 
 export default function UserSync() {
